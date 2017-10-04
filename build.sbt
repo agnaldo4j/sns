3bdf8f710a62d5a2eefb784b9b4cbabff7b81fea
Agnaldo de Oliveira <agnaldo4j@gmail.com>
2017-10-04 15:54:01 -0300

    remove Cammel dependency

--- build.sbt
+++ build.sbt
@@ -14,15 +14,14 @@ libraryDependencies ++= {
   Seq(
     "com.typesafe.akka" %% "akka-actor" % akkaVersion,
     "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-    "com.typesafe.akka" %% "akka-http-experimental" % akkaVersion,
-    "com.typesafe.akka" %% "akka-http-xml-experimental" % akkaVersion,
-    "com.typesafe.akka" %% "akka-http-spray-json-experimental" % akkaVersion
+    "com.typesafe.akka" %% "akka-http" % "10.0.9",
+    "com.typesafe.akka" %% "akka-http-xml" % "10.0.9",
+    "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.9"
     ,
-    "com.typesafe.akka" %% "akka-http-testkit" % akkaVersion % Test,
+    "com.typesafe.akka" %% "akka-http-testkit" % "10.0.9" % Test,
 
     "org.slf4j" % "slf4j-api" % "1.7.2",
     "ch.qos.logback" % "logback-classic" % "1.0.7",
-    "com.typesafe.akka" %% "akka-camel" % akkaVersion,
     "org.scalatest" %% "scalatest" % "3.0.0" % Test
   )
 }
--- src/main/scala/me/snov/sns/actor/ProducerActor.scala
+++ src/main/scala/me/snov/sns/actor/ProducerActor.scala
@@ -1,26 +1,30 @@
 package me.snov.sns.actor
 
 import akka.actor.{Actor, ActorLogging, Props}
-import akka.camel.{CamelMessage,Oneway}
-
+import akka.http.scaladsl.model.HttpResponse
 import spray.json._
-
 import me.snov.sns.model.Message
 
 object ProducerActor {
   def props(endpoint: String, subscriptionArn: String, topicArn: String) = Props(classOf[ProducerActor], endpoint, subscriptionArn, topicArn)
 }
 
-class ProducerActor(endpoint: String, subscriptionArn: String, topicArn: String) extends Actor with Oneway with ActorLogging {
+class ProducerActor(endpoint: String, subscriptionArn: String, topicArn: String) extends Actor with ActorLogging {
   def endpointUri = endpoint
 
-  override def transformOutgoingMessage(msg: Any) = msg match {
-    case snsMsg: Message => new CamelMessage(snsMsg.toJson.toString, Map(
-        CamelMessage.MessageExchangeId -> snsMsg.uuid,
-        "x-amz-sns-message-type" -> "Notification",
-        "x-amz-sns-message-id" -> snsMsg.uuid,
-        "x-amz-sns-subscription-arn" -> subscriptionArn,
-        "x-amz-sns-topic-arn" -> topicArn
-      ))
+  def transformOutgoingMessage(msg: Any) = msg match {
+    case snsMsg: Message => snsMsg.toJson.toString
+
+//    case snsMsg: Message => new CamelMessage(snsMsg.toJson.toString, Map(
+//        CamelMessage.MessageExchangeId -> snsMsg.uuid,
+//        "x-amz-sns-message-type" -> "Notification",
+//        "x-amz-sns-message-id" -> snsMsg.uuid,
+//        "x-amz-sns-subscription-arn" -> subscriptionArn,
+//        "x-amz-sns-topic-arn" -> topicArn
+//      ))
+  }
+
+  override def receive = {
+    case message: Message => sender ! transformOutgoingMessage(message)
   }
 }
